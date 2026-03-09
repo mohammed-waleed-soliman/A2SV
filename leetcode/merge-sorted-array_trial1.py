83a5dacd562556b0a27fbcc8ea115cf450e53eee
Mohammed Waleed <136664646+mohammed-waleed-soliman@users.noreply.github.com>
2026-03-09 17:18:42 +0200

    Add solution for Merge Sorted Array (trial 1)

---
+++ leetcode/merge-sorted-array_trial1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        res = []
+        i = 0
+        j = 0
+        while i<m and j<n:
+            if(nums1[i]<nums2[j]):
+                res.append(nums1[i])
+                i+=1
+            else:
+                res.append(nums2[j])
+                j+=1
+        while i<m:
+            res.append(nums1[i])
+            i+=1
+        while j<n:
+            res.append(nums2[j])
+            j+=1
+        for i in range(len(nums1)):
+            nums1[i]=res[i]
