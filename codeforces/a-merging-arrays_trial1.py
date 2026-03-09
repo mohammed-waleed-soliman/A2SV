840ec1b4ca6f25d0c61210845c5e6cd9747d87dd
Mohammed Waleed <136664646+mohammed-waleed-soliman@users.noreply.github.com>
2026-03-09 16:51:10 +0200

    Create a-merging-arrays_trial1.py

---
+++ codeforces/a-merging-arrays_trial1.py
@@ -0,0 +1,20 @@
+n, m = map(int,input().split())
+a = list(map(int,input().split()))
+b = list(map(int,input().split()))
+i = 0
+j = 0
+res = []
+while i<n and j<m:
+    if a[i]<b[j]:
+        res.append(a[i])
+        i+=1
+    else:
+        res.append(b[j])
+        j+=1
+while i<n:
+    res.append(a[i])
+    i+=1
+while j<m:
+    res.append(b[j])
+    j+=1
+print(*res)
