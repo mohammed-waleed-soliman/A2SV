91282e5ecd6c6848180e3c4727e499d404a3e394
Mohammed Waleed <136664646+mohammed-waleed-soliman@users.noreply.github.com>
2026-04-14 23:19:11 +0200

    Add solution for Reverse String (trial 1)

--- leetcode/reverse-string_trial1.py
+++ leetcode/reverse-string_trial1.py
@@ -1,7 +1,3 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        for i in range(len(s)//2):
-            temp = s[i]
-            s[i] = s[len(s)-i-1]
-            s[len(s)-i-1] = temp
-        
+        s.reverse()
